d9fe3abb52c06796de53d94cb8c6e06d9beaf415
atriumrules <102473631+atriumrules@users.noreply.github.com>
2023-02-04 18:41:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import discord, os, keep_alive, asyncio, datetime, pytz
+import discord, os, asyncio, datetime, pytz
 
 
 from discord.ext import tasks, commands
 
 client = commands.Bot(
   command_prefix=':',
-  self_bot=True
+  self_bot=False
 )
 
-
-keep_alive.keep_alive()
-client.run(os.getenv("TOKEN"), bot=False)
+client.run(os.getenv("MTA2MzQ3NTkxODg0NjI5NjE0NQ.GVKty2.wz_PsQO31An_Ja4LvI-bGAjGb1S3KRs29XQRG0"), bot=False)
